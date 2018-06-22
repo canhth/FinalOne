ca25106dcd24a0f7343a6a99b3d874d9adfdb5e6
Canh Tran <canh.tran@wizeline.com>
2018-06-22 10:18:41 +0700

    Create git ignore

--- FinalOne/Sections/MainHome/Servieces/HomeServiece.swift
+++ FinalOne/Sections/MainHome/Servieces/HomeServiece.swift
@@ -12,19 +12,44 @@ import CT_RESTAPI
 typealias HomePageServiceCompletionHandler = (_ results: [Item], _ error: RESTError?) -> Void
 
 protocol HomePageServiceProtocol {
-    func getListDrivers(completion: @escaping HomePageServiceCompletionHandler)
+    func getListAppleItems(completion: @escaping HomePageServiceCompletionHandler)
+    func getListAndroidItems(completion: @escaping HomePageServiceCompletionHandler)
 }
 
 final class HomePageServiece: HomePageServiceProtocol {
     
-    /// Get list of items
+    /// Get list Apple's items
     ///
     /// - Parameters:
     ///   - completion: Results and error of API
     /// - Returns: <[Item]>
-    func getListDrivers(completion: @escaping HomePageServiceCompletionHandler) {
+    func getListAppleItems(completion: @escaping HomePageServiceCompletionHandler) {
         
-        let apiManager = RESTApiClient(subPath: "wunderbucket", functionName: "locations.json", method: .GET, endcoding: .URL)
+        let apiManager = RESTApiClient(subPath: "", functionName: "apple.json", method: .GET, endcoding: .URL)
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+        apiManager.baseRequest { (results, error) in
+            if let jsonData = results as? Data {
+                do {
+                    let results: Products = try JSONDecoder().decode(Products.self, from: jsonData)
+                    completion(results.products, nil)
+                } catch {
+                    print("Error when parsing JSON: \(error)")
+                }
+            } else {
+                completion([], error)
+            }
+            UIApplication.shared.isNetworkActivityIndicatorVisible = false
+        }
+    }
+    
+    /// Get list Android's items
+    ///
+    /// - Parameters:
+    ///   - completion: Results and error of API
+    /// - Returns: <[Item]>
+    func getListAndroidItems(completion: @escaping HomePageServiceCompletionHandler) {
+        
+        let apiManager = RESTApiClient(subPath: "", functionName: "apple.json", method: .GET, endcoding: .URL)
         UIApplication.shared.isNetworkActivityIndicatorVisible = true
         apiManager.baseRequest { (results, error) in
             if let jsonData = results as? Data {
